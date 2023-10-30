07ef0b7f2863a0368336869e5bff1abc21846f2a
Devansh-Singh09 <139249720+Devansh-Singh09@users.noreply.github.com>
2023-10-30 22:33:21 +0530

    Update prefix.cpp

--- prefix.cpp
+++ prefix.cpp
@@ -1,44 +1,103 @@
-//Prefix
-int calc(int v1,int v2,char op){
-    if (op=='^')
-    {
-    return pow(v1,v2);
-    }else if (op=='+')
-    {
-    return v1+v2;
-    }else if (op=='-')
-    {
-    return v1-v2;
-    }else if (op=='*')
-    {
-    return v1*v2;
-    }else if (op=='/')
-    {
-    return v1/v2;
-    } 
+#include <bits/stdc++.h>
+using namespace std;
+bool isOperator(char c)
+{
+	return (!isalpha(c) && !isdigit(c));
 }
-int evaluate(string str){
-    stack<int> st;
-    for (int i = str.size()-1; i >= 0; i--)
-    {
-    char ch=str[i];
-    if (isdigit(ch))
-    {
-    st.push(ch-'0');
-//subtracting '0' to get original value of the number
-    }else{
-    int v2=st.top();
-    st.pop();
-    int v1=st.top();
-    st.pop();
-    st.push(calc(v1,v2,ch));
-    }
-    }
-    return st.top();}
-int main(){
-    string str="-9+*132";
-    cout<<evaluate(str);
-    return 0;
+
+int getPriority(char C)
+{
+	if (C == '-' || C == '+')
+		return 1;
+	else if (C == '*' || C == '/')
+		return 2;
+	else if (C == '^')
+		return 3;
+	return 0;
 }
+string infixToPostfix(string infix)
+{
+	infix = '(' + infix + ')';
+	int l = infix.size();
+	stack<char> char_stack;
+	string output;
+
+	for (int i = 0; i < l; i++) {
+		if (isalpha(infix[i]) || isdigit(infix[i]))
+			output += infix[i];
+		else if (infix[i] == '(')
+			char_stack.push('(');
+
+		else if (infix[i] == ')') {
+			while (char_stack.top() != '(') {
+				output += char_stack.top();
+				char_stack.pop();
+			}
+
+			char_stack.pop();
+		}
+		else {
+			if (isOperator(char_stack.top())) {
+				if (infix[i] == '^') {
+					while (
+						getPriority(infix[i])
+						<= getPriority(char_stack.top())) {
+						output += char_stack.top();
+						char_stack.pop();
+					}
+				}
+				else {
+					while (
+						getPriority(infix[i])
+						< getPriority(char_stack.top())) {
+						output += char_stack.top();
+						char_stack.pop();
+					}
+				}
+
+				char_stack.push(infix[i]);
+			}
+		}
+	}
+	while (!char_stack.empty()) {
+		output += char_stack.top();
+		char_stack.pop();
+	}
+	return output;
+}
+
+string infixToPrefix(string infix)
+{
+	int l = infix.size();
+
+	reverse(infix.begin(), infix.end());
+
+	for (int i = 0; i < l; i++) {
+
+		if (infix[i] == '(') {
+			infix[i] = ')';
+		}
+		else if (infix[i] == ')') {
+			infix[i] = '(';
+		}
+	}
+
+	string prefix = infixToPostfix(infix);
+
+	reverse(prefix.begin(), prefix.end());
+
+	return prefix;
+}
+
+int main()
+{
+	string s ;
+    cout<<"Enter the expression"<<endl;
+    cin>>s;
+    cout<<"Prefix expression:"<<endl;
+	cout << infixToPrefix(s) << std::endl;
+	return 0;
+}
+
 
 
